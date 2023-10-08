5144890166445c6930ed625861605632fca4a54c
Lahiru Amarasooriya <lahirudx@gmail.com>
2023-10-08 18:48:19 +0530

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Lahiru Amarasooriya on 2023-10-08.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
